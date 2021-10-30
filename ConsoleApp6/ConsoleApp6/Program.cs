645767b79d9d0956a8c12f8a0da31d22aa6148b5
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-30 18:43:03 +0200

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 4;
+            x = (y -= 2);
+            x = y++;
+            x = y--;
+            Console.WriteLine(x);
+            Console.ReadKey();
+            // bedzie miala wartosc 3
+        }
+    }
+}
