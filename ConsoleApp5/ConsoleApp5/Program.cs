5437e1f793f57e3ee5d8c23a238cf65bed9b028d
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-30 18:30:44 +0200

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2, y = 3;
+            x *= y * 2;
+            Console.WriteLine(x);
+            Console.ReadKey();
+            //odpowiedz D  x = 12 
+        }
+    }
+}
