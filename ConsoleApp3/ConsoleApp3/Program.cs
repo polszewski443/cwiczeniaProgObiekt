1d37711981958269ef7b663210c23823e350cad5
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-30 15:36:38 +0200

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double wzrost, masa, BMI;
+            Console.WriteLine("Podaj wzrost w metrach i mase w kilogramach, aby program mogl obliczyc wskaznik BMI.");
+            Console.WriteLine("Wzrost: ");
+            wzrost = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Masa: ");
+            masa = Convert.ToDouble(Console.ReadLine());
+            BMI = masa / wzrost;
+            Console.WriteLine(wzrost);
+            Console.WriteLine("BMI wynosi: {0}", BMI);
+            Console.ReadKey();
+        }
+    }
+}
