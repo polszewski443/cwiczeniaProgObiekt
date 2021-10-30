0b50dfd360c0aadf30d2ccce21de6948082a2ba1
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-30 13:17:54 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double F, C;
+            Console.WriteLine("Podaj temp. w stopniach Celcjusza");
+            C = double.Parse(Console.ReadLine());
+            F = 9.0 / 5 * C + 32;
+            Console.WriteLine(F);
+            Console.ReadKey();
+        }
+    }
+}
