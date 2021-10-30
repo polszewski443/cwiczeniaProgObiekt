8df67156f65ae35051081d3c563472dfe90a2b4d
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-30 19:28:46 +0200

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int powierzchnia = 100, osoby = 10;
+            double gestoscZaludnienia = (double)osoby / powierzchnia; 
+            Console.WriteLine(gestoscZaludnienia);
+            Console.ReadKey();
+        }
+    }
+}
