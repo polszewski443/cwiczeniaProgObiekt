85396c42cdbb609613baf909f6b54ea79224fc5e
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-30 18:51:19 +0200

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x;
+            int y = 1, z = 1;
+            x = (y == 1 && z++ == 1);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.ReadKey();
+            // odpowiedz A x=true y=1 z=2
+        }
+    }
+}
