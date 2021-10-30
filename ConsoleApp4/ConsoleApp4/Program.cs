548e2d030c4c37c96d84fcdb5a90e86c82f44a06
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-30 15:40:56 +0200

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100; 
+            Console.WriteLine(++x * 2);
+            //202 wyjdzie, odp A
+        }
+    }
+}
