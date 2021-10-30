52856a268b6a5f5287e2b370c27a120469f7c048
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-10-30 18:47:10 +0200

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 5;
+            x = ++y * 2;
+            x = y++;
+            x = y--;
+            Console.WriteLine(++y);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+            //wynik to x = 7 y = 7
+        }
+    }
+}
