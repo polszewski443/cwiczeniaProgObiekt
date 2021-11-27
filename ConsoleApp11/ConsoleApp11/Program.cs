ec4938843abb4b8105336dc19847e6fe7494eb04
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-11-27 21:31:35 +0100

    Add files via upload
    3.1

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rok a program sprawdzi czy jest przestepny: ");
+            int rok = int.Parse(Console.ReadLine());
+            if ((rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0)
+            {
+                Console.WriteLine("Rok jest przestepny");
+            }
+            else 
+                Console.WriteLine("Rok nie jest przestepny");
+            Console.ReadKey();
+        }
+    }
+}
