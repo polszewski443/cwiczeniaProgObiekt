a9307332a09f53decbdee74efcfd1027c48d9c0a
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-11-28 19:59:33 +0100

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("Podaj liczbe calkowitą a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj liczbe calkowitą b: ");
+            b = int.Parse(Console.ReadLine());
+            if (a % b == 0)
+            {
+                Console.WriteLine("{0} jest dzielnikiem liczby {1}", b, a);
+            }
+            else
+            {
+                if (b % a == 0)
+                {
+                    Console.WriteLine("{0} jest dzielnikiem liczby {1}", a, b);
+                }
+                else Console.WriteLine("Nie jest dzielnikiem");
+            }
+
+        }
+    }
+}
