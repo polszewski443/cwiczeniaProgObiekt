e09152ab7e2ec484359565842acc38f1207195c7
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-11-28 20:03:06 +0100

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BMI, wzrost, masa;
+            Console.WriteLine("Podaj wzrost (m) ");
+            wzrost = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj mase (kg): ");
+            masa = double.Parse(Console.ReadLine());
+            BMI = masa / (wzrost * 2);
+            Console.WriteLine("BMI:{0:F2}", BMI);
+            Console.ReadKey();
+        }
+
+    }
+}
