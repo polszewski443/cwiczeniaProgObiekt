b4af5d7557adac866a3cebe770ed696fb4f12ea0
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-12-02 23:09:44 +0100

    Add files via upload

---
+++ ConsoleApp31/ConsoleApp31/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ConsoleApp31
+{
+    class Program
+    {
+        static double Zamiana(double a)
+        {
+            double wynik = (5d / 9) * (a - 32);
+            return wynik;
+        }
+        static void Main(string[] args)
+        {
+            double fahrenhaity;
+            Console.WriteLine("Podaj temperature w stopniach Fahrenheita");
+            fahrenhaity = double.Parse(Console.ReadLine());
+            Console.WriteLine("Wynik to w stopniach Celsjusza = {0}", Zamiana(fahrenhaity));
+            Console.ReadKey();
+        }
+    }
+}
