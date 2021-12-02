7b7c23610cb1b7f5f86bb271bd7b93cc91e7baa0
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-12-02 22:48:19 +0100

    Add files via upload

---
+++ ConsoleApp26/ConsoleApp26/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[,] tab = new int[,]  {{ 1, 2, 3 , 4, 5},
+                                      {6, 7, 8, 9, 20},
+                                      {21, 22, 23, 24, 25},
+                                      {5, 5, 2, 2, 1},
+                                      {3, 2, 3, 2, 3}};
+            for (int a = 0; a < 5; a++)
+            {
+                for (int b = 0; b < 5; b++)
+                {
+                    Console.Write("{0,3}", tab[a, b]);
+                }
+                Console.WriteLine();
+            }
+            int suma = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == j)
+                    {
+                        suma = suma + tab[i, j];
+                    }
+                }
+            }
+            Console.Write("Suma:{0}", suma);
+            Console.ReadKey();
+        }
+    }
+}
