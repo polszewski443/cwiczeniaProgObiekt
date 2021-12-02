1c315a60e93283acca93a29a07badceb6b3125b6
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-12-02 20:59:55 +0100

    Add files via upload

---
+++ ConsoleApp19a/ConsoleApp19a/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp19a
+{ }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Podaj l wierszy: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)  
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j <= i) Console.Write("*");
+                    else Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
