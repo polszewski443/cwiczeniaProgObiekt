1241882ec5cb6e40d7a6a130e6e4428bf349f76d
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-12-02 21:12:06 +0100

    Add files via upload

---
+++ ConsoleApp21/ConsoleApp21/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj rozmiar tablicy: ");
+            int rozmiar = int.Parse(Console.ReadLine());
+            int[] tab = new int[rozmiar];
+
+            for (int i = 0; i < rozmiar; i++)
+            {
+                Console.WriteLine("Podaj wartosc {0}", i + 1);
+                int wartosc = Convert.ToInt32(Console.ReadLine());
+                tab[i] = wartosc;
+            }
+            for (int j = 0; j < tab.Length; j++)
+            {
+                Console.WriteLine("Index: {0} Wart: {1}", j + 1, tab[j]);
+            }
+
+        }
+    }
+}
