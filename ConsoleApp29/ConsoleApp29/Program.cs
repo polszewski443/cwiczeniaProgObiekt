a706d8e1374c77b2f9e35203dd64521e07c77dd3
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-12-02 23:02:27 +0100

    Add files via upload

--- ConsoleApp29/ConsoleApp29/Program.cs
+++ ConsoleApp29/ConsoleApp29/Program.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 
+
 namespace ConsoleApp29
 {
     class Program
     {
-        public static void Main()
+        static double Zamiana(double a)
         {
-            Console.WriteLine("Napisz jakies zdanie: ");
-            string zdanie = Console.ReadLine();
-            int i = 0;
-            int ile = 0;
-            while (i <= zdanie.Length - 1)
-            {
-                if (zdanie[i] == ' ' || zdanie[i] == '\t')
-                {
-                    ile++;
-                }
-                i++;
-            }
-            Console.WriteLine("Ilosc wyrazow = " + (ile + 1));
-            Console.ReadLine();
+            double wynik = (5d / 9) * (a - 32);
+            return wynik;
+        }
+        static void Main(string[] args)
+        {
+            double fahrenhaity;
+            Console.WriteLine("Podaj temperature w stopniach Fahrenheita");
+            fahrenhaity = double.Parse(Console.ReadLine());
+            Console.WriteLine("Wynik to w stopniach Celsjusza = {0}", Zamiana(fahrenhaity));
+            Console.ReadKey();
         }
     }
 }
