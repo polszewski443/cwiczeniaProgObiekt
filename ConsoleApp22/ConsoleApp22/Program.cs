9a93989d51fa50ff1448e70661d105199611eac0
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-12-02 21:58:10 +0100

    Add files via upload

---
+++ ConsoleApp22/ConsoleApp22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp22
+{ 
+    class Program
+    {
+        public static void Main()
+        {
+            int[] a = { 3, -2, -9, 1, 42, -14, -43, -22, 15, 8 };
+            int[] b = new int[10];
+            int ile = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > 0)
+                {
+                    b[ile] = a[i];
+                    ile++;
+                }
+            }
+            Console.Write(string.Join(", ", b));
+        }
+    }
+}
