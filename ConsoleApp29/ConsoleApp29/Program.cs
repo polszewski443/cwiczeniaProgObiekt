a50c6dbe975cd4c54b565df31354d882e3895c39
polszewski443 <70515373+polszewski443@users.noreply.github.com>
2021-12-02 22:56:44 +0100

    Add files via upload

---
+++ ConsoleApp29/ConsoleApp29/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Napisz jakies zdanie: ");
+            string zdanie = Console.ReadLine();
+            int i = 0;
+            int ile = 0;
+            while (i <= zdanie.Length - 1)
+            {
+                if (zdanie[i] == ' ' || zdanie[i] == '\t')
+                {
+                    ile++;
+                }
+                i++;
+            }
+            Console.WriteLine("Ilosc wyrazow = " + (ile + 1));
+            Console.ReadLine();
+        }
+    }
+}
